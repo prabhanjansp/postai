6c62ffb2217c6338ba075c15df83bb9606804ba8
prabhanjansp <puranikchetan97@gmail.com>
2025-02-10 16:32:22 +0530

    New React Native app

---
+++ app/index.jsx
@@ -0,0 +1,46 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import "react-native-reanimated";
+import { Link } from "expo-router";
+
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { Text } from "react-native";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+
+      <Text style={{ textAlign: "center", marginTop: 100 }}>Jrlll</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile" style={{ color: "red" }}>
+        GO to Profile page
+      </Link>
+    </ThemeProvider>
+  );
+}
